4483799edee9e05258452bdd6533dccb1650376d
Azamat Omirtai <omirtajazamat@gmail.com>
2026-02-02 17:16:53 +0500

    [feat]: complete exam platform implementation with dark mode and tests

--- tests/QuestionCard.test.tsx
+++ tests/QuestionCard.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import { describe, it, expect, vi } from 'vitest'
-import QuestionCard from './QuestionCard'
+import QuestionCard from '../components/question-card/QuestionCard'
 
 const mockQuestion = {
   id: '1',
